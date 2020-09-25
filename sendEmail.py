7bfc59d034c48cf9d49a4c14704d6bb2c2c19f9c
Vasiliy Sharapov <vasiliys@gmail.com>
2020-09-25 08:34:11 +0000

    Set custom name in sent email
    Found a workaround for unicode names: https://stackoverflow.com/a/44737244/399257

--- sendEmail.py
+++ sendEmail.py
@@ -2,18 +2,19 @@
 
 import smtplib
 
+fromName = 'Вася'
 fromAddress = "vasiliy@vas.im"
 toAddressList = ["vasiliys@gmail.com"]
 subject = "Hello!"
 body = "This message was sent with Python's smtplib."
 message = """\
-From: %s
+From: %s <%s>
 To: %s
 Subject: %s
 
 %s
-""" % (fromAddress, ", ".join(toAddressList), subject, body)
+""" % (fromName, fromAddress, ", ".join(toAddressList), subject, body)
 
 server = smtplib.SMTP('localhost')
-server.sendmail(fromAddress, toAddressList, message)
+server.sendmail(fromAddress, toAddressList, message.encode('utf-8'))
 server.quit()
